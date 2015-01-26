d182a70e61379a437540c7231180d420f0eb3527
Diego alejandro Franco osorio <diegoalejandrofrancoosorio@mbp-de-diego>
2015-01-26 09:16:08 -0500

    Esta el popup listo pero me falta el estilo  y el guion espero eso para terminarlo

--- uno27/index.php
+++ uno27/index.php
@@ -1,95 +1,97 @@
 <html>
 
-<head>
-  <title>Agcitmed.Net</title>
+    <head>
+        <title>Agcitmed.Net</title>
 
-  <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1"/>
-  <meta http-equiv="Content-Script-Type" content="text/javascript"/>
-  <meta name="Generator" content="Corel PHOTO-PAINT 16.1"/>
-  <link rel="stylesheet" type="text/css" href="site.css">
-<script src="librerias/modernizr-2.6.2.js" type="text/javascript"></script>
-<script src="librerias/jquery-1.9.0.min.js" type="text/javascript"></script>
-<script src="librerias/jquery-ui-1.9.2.min.js" type="text/javascript"></script>
-<script src="librerias/js/cufon-yui.js" type="text/javascript"></script>
-<script src="librerias/js/cufon-replace.js" type="text/javascript"></script>
-<script src="librerias/js/Asap_400.font.js" type="text/javascript"></script>
-<script src="librerias/js/Asap_italic_400.font.js" type="text/javascript"></script>
-<script src="librerias/js/FF-cash.js" type="text/javascript"></script>
-<script src="librerias/js/jquery.equalheights.js" type="text/javascript"></script>
-<script src="librerias/js/jquery.cycle.all.js" type="text/javascript"></script>
-<script src="librerias/uno27.js" type="text/javascript"></script>
-<script src="librerias/jquery.bpopup.min.js" type="text/javascript"></script>
-</head>
-<body>
-<div id="element_to_pop_up">Este es el mensaje del pop up</div>
-<table border="0" cellspacing="0" cellpadding="0" width="1093" height="615" align="center">
-  <tr align="left" valign="top">
-    <td rowspan="1" colspan="1" height="1" width="1"></td>
-    <td rowspan="1" colspan="1" height="1" width="218"></td>
-    <td rowspan="1" colspan="1" height="1" width="86"></td>
-    <td rowspan="1" colspan="1" height="1" width="212"></td>
-    <td rowspan="1" colspan="1" height="1" width="20"></td>
-    <td rowspan="1" colspan="1" height="1" width="333"></td>
-    <td rowspan="1" colspan="1" height="1" width="223"></td>
-  </tr>
-  <tr align="left" valign="top">
-    <td rowspan="1" colspan="1" width="1" height="90"></td>
-    <td rowspan="2" colspan="4" width="536" height="132"><img border="0" width="536" height="132" src="imagenes/loggin1.jpg" alt=""></td>
-    <td rowspan="1" colspan="2" width="556" height="90"><img border="0" width="556" height="90" src="imagenes/loggin2.jpg" alt=""></td>
-  </tr>
-  <tr align="left" valign="top">
-    <td rowspan="1" colspan="1" width="1" height="42"></td>
-    <td width="556" height="42" colspan="2" rowspan="1" valign="middle" background="imagenes/loggin3.jpg">
-	<ul id="menu">
-		<li class="link"><a href="#" class="a">Citas Medicas</a></li>
-		<li class="link"><a href="#" class="a">Reportes</a></li>
-                <li class="link"><a href="#" class="a">Administraci&oacute;n</a></li>
-		<li class="link"><a href="#" class="a">PQR</a></li>
-	</ul>	</td>
-  </tr>
-  <tr align="left" valign="top">
-    <td rowspan="1" colspan="1" width="1" height="114"></td>
-    <td rowspan="5" colspan="2" width="304" height="319"><img border="0" width="304" height="319" src="imagenes/loggin4.jpg" alt=""></td>
-    <td rowspan="1" colspan="1" width="212" height="114"><img border="0" width="212" height="114" src="imagenes/loggin5.jpg" alt=""></td>
-    <td rowspan="5" colspan="3" width="576" height="319"><img border="0" width="576" height="319" src="imagenes/loggin10.jpg" alt=""></td>
-  </tr>
-  <tr align="left" valign="top">
-    <td rowspan="1" colspan="1" width="1" height="38"></td>
-    <td rowspan="1" colspan="1" width="212" height="38" background="imagenes/loggin6.jpg"><input type="text" name="txtUsuario" placeholder="Usuario" id="txtUsuario">    </td>
-  </tr>
-  <tr align="left" valign="top">
-    <td rowspan="1" colspan="1" width="1" height="29"></td>
-    <td rowspan="1" colspan="1" width="212" height="29"><img border="0" width="212" height="29" src="imagenes/loggin7.jpg" alt=""></td>
-  </tr>
-  <tr align="left" valign="top">
-    <td rowspan="1" colspan="1" width="1" height="38"></td>
-    <td rowspan="1" colspan="1" width="212" height="38" background="imagenes/loggin8.jpg"><input type="password" placeholder="Contrase&nacute;a" name="txtPasswd" id="txtPasswd"></td>
-  </tr>
-  <tr align="left" valign="top">
-    <td rowspan="1" colspan="1" width="1" height="100"></td>
-    <td rowspan="1" colspan="1" width="212" height="100" background="imagenes/loggin9.png"align="center" valign="middle">
-      <input type="button" name="btnIngresar" value="Ingresar" id="btnIngresar" onClick="citmed()" class="button">    </td>
-  </tr>
-  <tr align="left" valign="top">
-    <td rowspan="1" colspan="1" width="1" height="86"></td>
-    <td rowspan="1" colspan="1" width="218" height="86"><img border="0" width="218" height="86" src="imagenes/loggin11.jpg" alt=""></td>
-    <td rowspan="1" colspan="4" width="651" height="86" background="imagenes/loggin12.jpg">
-      <div class="wrapper">
-	  <div id="banners" class="border-bot">
-              <div> <strong class="title-1">Agende sus citas</strong> <strong class="title-2">R&aacute;pido, f&aacute;cil y al instante</strong> </div>
-        <div> <strong class="title-1">Estad&iacute;sticas</strong> <strong class="title-2">Conozca la efectividad de su equipo</strong> </div>
-        <div> <strong class="title-1">Tome decisiones</strong> <strong class="title-2">Ap&oacute;yese en nuestros reportes para tomar acciones</strong> </div>
-      </div>
-	  <a id="prev" href="#">prev</a>
-	  <a id="next" href="#">next</a>
-	  </div>
-	  </td>
-    <td rowspan="1" colspan="1" width="223" height="86"><img border="0" width="223" height="86" src="imagenes/loggin13.jpg" alt=""></td>
-  </tr>
-  <tr align="left" valign="top">
-    <td rowspan="1" colspan="1" width="1" height="77"></td>
-    <td rowspan="1" colspan="6" width="1092" height="77"><img border="0" width="1092" height="77" src="imagenes/loggin14.jpg" alt=""></td>
-  </tr>
-</table>
-</body>
+        <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1"/>
+        <meta http-equiv="Content-Script-Type" content="text/javascript"/>
+        <meta name="Generator" content="Corel PHOTO-PAINT 16.1"/>
+        <link rel="stylesheet" type="text/css" href="site.css">
+        <script src="librerias/modernizr-2.6.2.js" type="text/javascript"></script>
+        <script src="librerias/jquery-1.9.0.min.js" type="text/javascript"></script>
+        <script src="librerias/jquery-ui-1.9.2.min.js" type="text/javascript"></script>
+        <script src="librerias/js/cufon-yui.js" type="text/javascript"></script>
+        <script src="librerias/js/cufon-replace.js" type="text/javascript"></script>
+        <script src="librerias/js/Asap_400.font.js" type="text/javascript"></script>
+        <script src="librerias/js/Asap_italic_400.font.js" type="text/javascript"></script>
+        <script src="librerias/js/FF-cash.js" type="text/javascript"></script>
+        <script src="librerias/js/jquery.equalheights.js" type="text/javascript"></script>
+        <script src="librerias/js/jquery.cycle.all.js" type="text/javascript"></script>
+        <script src="librerias/uno27.js" type="text/javascript"></script>
+        <script src="librerias/jquery.bpopup.min.js" type="text/javascript"></script>
+    </head>
+    <body>
+        <div id="element_to_pop_up">Este es el mensaje del pop up
+            <a class="b-close">x<a/>
+        </div>
+        <table border="0" cellspacing="0" cellpadding="0" width="1093" height="615" align="center">
+            <tr align="left" valign="top">
+                <td rowspan="1" colspan="1" height="1" width="1"></td>
+                <td rowspan="1" colspan="1" height="1" width="218"></td>
+                <td rowspan="1" colspan="1" height="1" width="86"></td>
+                <td rowspan="1" colspan="1" height="1" width="212"></td>
+                <td rowspan="1" colspan="1" height="1" width="20"></td>
+                <td rowspan="1" colspan="1" height="1" width="333"></td>
+                <td rowspan="1" colspan="1" height="1" width="223"></td>
+            </tr>
+            <tr align="left" valign="top">
+                <td rowspan="1" colspan="1" width="1" height="90"></td>
+                <td rowspan="2" colspan="4" width="536" height="132"><img border="0" width="536" height="132" src="imagenes/loggin1.jpg" alt=""></td>
+                <td rowspan="1" colspan="2" width="556" height="90"><img border="0" width="556" height="90" src="imagenes/loggin2.jpg" alt=""></td>
+            </tr>
+            <tr align="left" valign="top">
+                <td rowspan="1" colspan="1" width="1" height="42"></td>
+                <td width="556" height="42" colspan="2" rowspan="1" valign="middle" background="imagenes/loggin3.jpg">
+                    <ul id="menu">
+                        <li class="link"><a href="#" class="a">Citas Medicas</a></li>
+                        <li class="link"><a href="#" class="a">Reportes</a></li>
+                        <li class="link"><a href="#" class="a">Administraci&oacute;n</a></li>
+                        <li class="link"><a href="#" class="a">PQR</a></li>
+                    </ul>	</td>
+            </tr>
+            <tr align="left" valign="top">
+                <td rowspan="1" colspan="1" width="1" height="114"></td>
+                <td rowspan="5" colspan="2" width="304" height="319"><img border="0" width="304" height="319" src="imagenes/loggin4.jpg" alt=""></td>
+                <td rowspan="1" colspan="1" width="212" height="114"><img border="0" width="212" height="114" src="imagenes/loggin5.jpg" alt=""></td>
+                <td rowspan="5" colspan="3" width="576" height="319"><img border="0" width="576" height="319" src="imagenes/loggin10.jpg" alt=""></td>
+            </tr>
+            <tr align="left" valign="top">
+                <td rowspan="1" colspan="1" width="1" height="38"></td>
+                <td rowspan="1" colspan="1" width="212" height="38" background="imagenes/loggin6.jpg"><input type="text" name="txtUsuario" placeholder="Usuario" id="txtUsuario">    </td>
+            </tr>
+            <tr align="left" valign="top">
+                <td rowspan="1" colspan="1" width="1" height="29"></td>
+                <td rowspan="1" colspan="1" width="212" height="29"><img border="0" width="212" height="29" src="imagenes/loggin7.jpg" alt=""></td>
+            </tr>
+            <tr align="left" valign="top">
+                <td rowspan="1" colspan="1" width="1" height="38"></td>
+                <td rowspan="1" colspan="1" width="212" height="38" background="imagenes/loggin8.jpg"><input type="password" placeholder="Contrase&nacute;a" name="txtPasswd" id="txtPasswd"></td>
+            </tr>
+            <tr align="left" valign="top">
+                <td rowspan="1" colspan="1" width="1" height="100"></td>
+                <td rowspan="1" colspan="1" width="212" height="100" background="imagenes/loggin9.png"align="center" valign="middle">
+                    <input type="button" name="btnIngresar" value="Ingresar" id="btnIngresar"  class="button">    </td>
+            </tr>
+            <tr align="left" valign="top">
+                <td rowspan="1" colspan="1" width="1" height="86"></td>
+                <td rowspan="1" colspan="1" width="218" height="86"><img border="0" width="218" height="86" src="imagenes/loggin11.jpg" alt=""></td>
+                <td rowspan="1" colspan="4" width="651" height="86" background="imagenes/loggin12.jpg">
+                    <div class="wrapper">
+                        <div id="banners" class="border-bot">
+                            <div> <strong class="title-1">Agende sus citas</strong> <strong class="title-2">R&aacute;pido, f&aacute;cil y al instante</strong> </div>
+                            <div> <strong class="title-1">Estad&iacute;sticas</strong> <strong class="title-2">Conozca la efectividad de su equipo</strong> </div>
+                            <div> <strong class="title-1">Tome decisiones</strong> <strong class="title-2">Ap&oacute;yese en nuestros reportes para tomar acciones</strong> </div>
+                        </div>
+                        <a id="prev" href="#">prev</a>
+                        <a id="next" href="#">next</a>
+                    </div>
+                </td>
+                <td rowspan="1" colspan="1" width="223" height="86"><img border="0" width="223" height="86" src="imagenes/loggin13.jpg" alt=""></td>
+            </tr>
+            <tr align="left" valign="top">
+                <td rowspan="1" colspan="1" width="1" height="77"></td>
+                <td rowspan="1" colspan="6" width="1092" height="77"><img border="0" width="1092" height="77" src="imagenes/loggin14.jpg" alt=""></td>
+            </tr>
+        </table>
+    </body>
 </html>
---
+++ uno27/librerias/uno27.js
@@ -0,0 +1,94 @@
+jQuery(document).ready(function () {
+    $('#banners')
+            .cycle({
+                fx: 'fade',
+                delay: 4000,
+                timeout: 4000,
+                manualTrump: false,
+                cleartypeNoBg: true,
+                next: '#next',
+                prev: '#prev'
+            });
+    $("#btnBuscar").click(function () {
+        validartelefono();
+    });
+    $("#btnHU").click(function () {
+        $("#paso1").hide();
+        $("#paso2").show();
+    });
+    $("#btnCE").click(function () {
+        $("#paso1").hide();
+        $("#paso5").show();
+    });
+    $("#btn1").click(function () {
+        $("#paso2").hide();
+        $("#paso3").show();
+        $("#paso3-1").show();
+    });
+    $("#btn3").click(function () {
+        $("#paso3").hide();
+        $("#paso4").show();
+    });
+    $("#btn5").click(function () {
+        $("#paso4").hide();
+        $("#paso3-1").hide();
+        location.reload();
+    });
+    $("#btn4").click(function () {
+        $("#paso6").show();
+    });
+    $("#select1").change(function () {
+        $("#paso6").show();
+    });
+
+});
+function citmed() {
+    if (($("#txtUsuario").val() == 'james') && ($("#txtPasswd").val() == 'abc123')) {
+        window.location.assign("citmed.php")
+    }
+    else {
+        bootbox.alert("hola");
+    }
+}
+
+;
+(function ($) {
+
+    // DOM Ready
+    $(function () {
+
+        // Binding a click event
+        // From jQuery v.1.7.0 use .on() instead of .bind()
+        $('#btnIngresar').bind('click', function (e) {
+            if (($("#txtUsuario").val() == 'james') && ($("#txtPasswd").val() == 'abc123')) {
+                window.location.assign("citmed.php")
+            }
+            // Prevents the default action to be triggered. 
+            else {
+            // Prevents the default action to be triggered. 
+                e.preventDefault();
+
+                // Triggering bPopup when click event is fired
+                $('#element_to_pop_up').bPopup({
+	    easing: 'easeOutBack', //uses jQuery easing plugin
+            speed: 450,
+            transition: 'slideDown'
+        });
+             
+        }
+                });
+
+    });
+
+})(jQuery);
+
+
+function validartelefono() {
+    if ($("#txtTelefono").val() == "") {
+    }
+    else {
+        $("#paso0").hide();
+        $("#paso1").show();
+    }
+}
+;
